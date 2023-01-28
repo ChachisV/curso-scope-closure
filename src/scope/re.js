1de5938a09d9b7b1a1b51f0c3860d30c5f98b71e
Jaime Andres Vanegas <javanmot@gmail.com>
2023-01-27 22:16:55 -0500

    aprendimos a reasignar y redeclarar asi como el strict mode

---
+++ src/scope/re.js
@@ -0,0 +1,35 @@
+var firstName; //Undefined
+firstName = "Jaime"
+console.log(firstName);
+
+var lastName = "Vanegas";// declarar asignar
+lastName = "Ana"; //reasignar
+console.log(lastName)
+
+var secondName = "Jaime"; // declarando / asignando
+var secondName = "Ana"; //reasignando
+console.log(secondName);
+
+// Let
+
+let fruit = "Apple" //declarar y asignar
+fruit = "Kiwi"; // reasignar
+console.log(fruit);
+
+let fruit = "Banana";
+
+
+// Const
+
+const animal = "Dog"; // declara y asigna
+animal = "Cat"; // Reasignando
+console.log(animal);
+const animal = "Snake";
+
+
+const vehicles = [];
+vehicles.push("Carrito");
+console.log(vehicles);
+
+vehicles.pop();
+console.log(vehicles);
