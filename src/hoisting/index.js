201058d83dd6b98a821855d48c11659d7664a171
Jaime Andres Vanegas <javanmot@gmail.com>
2023-08-04 13:27:45 -0500

    repasamos sobre el hoisting

---
+++ src/hoisting/index.js
@@ -0,0 +1,12 @@
+// console.log(nameOfDog);
+// var nameOfDog = 'Elmo';
+// console.log(nameOfDog);
+
+nameOfDog();
+
+function nameOfDog(){
+    console.log(`El mejor perrito es ${elmo}`);
+
+}
+
+var elmo = 'Elmo'
